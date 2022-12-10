994fc96f16b27a8f38854a652c863534fb559157
Adil Ismail <adilismail62@gmail.com>
2022-12-10 20:16:04 +0530

    Setup device, esbuild & react

---
+++ config/routes.rb
@@ -0,0 +1,23 @@
+Rails.application.routes.draw do
+  root to: "home#index"
+
+  namespace :api, defaults: { format: :json } do
+    resources :users, only: %w[show]
+  end
+
+  devise_for :users,
+    defaults: { format: :json },
+    path: '',
+    path_names: {
+      sign_in: 'api/login',
+      sign_out: 'api/logout',
+      registration: 'api/signup'
+    },
+    controllers: {
+      sessions: 'sessions',
+      registrations: 'registrations'
+    }
+  
+  get "*path", to: "home#index"
+  get "*path", to: "home#index"
+end
